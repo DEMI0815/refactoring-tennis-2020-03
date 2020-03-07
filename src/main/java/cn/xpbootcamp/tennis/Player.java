06e85adad79e794e28c63f9fdbcf76c450de5f67
Yidi <yidi.zhao@thoughtworks.com>
2020-03-07 14:44:05 +0800

    Encapsulate fields to Player class

---
+++ src/main/java/cn/xpbootcamp/tennis/Player.java
@@ -0,0 +1,22 @@
+package cn.xpbootcamp.tennis;
+
+public class Player {
+    String name;
+    int score;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void wonPoint() {
+        score++;
+    }
+}
--- src/main/java/cn/xpbootcamp/tennis/TennisGameImpl.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGameImpl.java
@@ -5,21 +5,19 @@ import java.util.Arrays;
 
 public class TennisGameImpl implements TennisGame {
 
-    private int player1Score = 0;
-    private int player2Score = 0;
-    private String player1Name;
-    private String player2Name;
+    private Player player1;
+    private Player player2;
 
     public TennisGameImpl(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.player1 = new Player(player1Name);
+        this.player2 = new Player(player2Name);
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1")) {
-            player1Score += 1;
+        if (player1.getName().equals(playerName)) {
+            player1.wonPoint();
         } else {
-            player2Score += 1;
+            player2.wonPoint();
         }
     }
 
@@ -35,60 +33,60 @@ public class TennisGameImpl implements TennisGame {
 
     private String scoreForAtLeastOneMoreThan4() {
         if (doesPlayer1HasAdvantage()) {
-            return scoreForAdvantage(player1Name);
+            return scoreForAdvantage(player1);
         }
         if (doesPlayer2HasAdvantage()) {
-            return scoreForAdvantage(player2Name);
+            return scoreForAdvantage(player2);
         }
         if (doesPlayer1Win()) {
-            return scoreForWin(player1Name);
+            return scoreForWin(player1);
         }
         if (doesPlayer2Win()) {
-            return scoreForWin(player2Name);
+            return scoreForWin(player2);
         }
         return "";
     }
 
     private String scoreForBothBelow4() {
-        return getScoreName(player1Score) + "-" + getScoreName(player2Score);
+        return getScoreName(player1.getScore()) + "-" + getScoreName(player2.getScore());
     }
 
-    private String scoreForWin(String playerName) {
-        return "Win for " + playerName;
+    private String scoreForWin(Player player) {
+        return "Win for " + player.getName();
     }
 
-    private String scoreForAdvantage(String playerName) {
-        return "Advantage " + playerName;
+    private String scoreForAdvantage(Player player) {
+        return "Advantage " + player.getName();
     }
 
     private boolean doesPlayer1Win() {
-        return player1Score - player2Score >= 2;
+        return player1.getScore() - player2.getScore() >= 2;
     }
 
     private boolean doesPlayer2Win() {
-        return player2Score - player1Score >= 2;
+        return player2.getScore() - player1.getScore() >= 2;
     }
 
     private boolean doesPlayer2HasAdvantage() {
-        return player1Score - player2Score == -1;
+        return player1.getScore() - player2.getScore() == -1;
     }
 
     private boolean doesPlayer1HasAdvantage() {
-        return player1Score - player2Score == 1;
+        return player1.getScore() - player2.getScore() == 1;
     }
 
     private boolean isAtLeastOneMoreThan4() {
-        return player1Score >= 4 || player2Score >= 4;
+        return player1.getScore() >= 4 || player2.getScore() >= 4;
     }
 
     private String scoreForEquals() {
         String score;
-        score = player1Score > 2 ? "Deuce" : getScoreName(player1Score) + "-All";
+        score = player1.getScore() > 2 ? "Deuce" : getScoreName(player1.getScore()) + "-All";
         return score;
     }
 
     private boolean isScoreEquals() {
-        return player1Score == player2Score;
+        return player1.getScore() == player2.getScore();
     }
 
     private String getScoreName(int score) {
